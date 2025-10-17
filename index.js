6def97ef1765621fbe0b62d1b26a8bed080a2a44
arturo320182-code <arturo320182@gmail.com>
2025-10-17 17:00:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.post("/webhook", (req, res) => {
-  console.log("Mensaje recibido:", req.body);
-  const twiml = \`
-    <Response>
-      <Message>¡Hola! Soy tu bot de InDrive 🟢</Message>
-    </Response>\`;
-  res.set("Content-Type", "text/xml");
-  res.send(twiml);
-});
 
 app.get("/", (req, res) => {
-  res.send("Bot InDrive activo ✅");
+  res.send("¡Bot InDrive funcionando correctamente!");
 });
 
 app.listen(port, () => {
-  console.log(\`Servidor iniciado en http://localhost:\${port}\`);
+  console.log(Servidor iniciado en http://localhost:${port});
 });
