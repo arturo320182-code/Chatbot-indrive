2d7344de55c9ac642a4ae927a7ea404f3c8af82a
arturo320182-code <arturo320182@gmail.com>
2025-10-17 16:28:39 -0400

    Add body-parser and update response messages

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("¡Bot InDrive funcionando correctamente!");
+  res.send("🚀 Bot InDrive funcionando correctamente!");
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("📨 Mensaje recibido:", req.body);
+
+  const twiml = `
+    <Response>
+      <Message>👋 ¡Hola! Soy tu bot InDrive. ¿En qué te puedo ayudar?</Message>
+    </Response>
+  `;
+
+  res.type("text/xml");
+  res.send(twiml);
 });
 
 app.listen(port, () => {
-  console.log(`Servidor iniciado en http://localhost:${port}`);
+  console.log(Servidor iniciado en http://localhost:${port});
 });
